5d716fb4e720a1a1d743e96e916f1c02444f773f
DmitriyKozyatinskiy <dmitriy.kozyatinskiy@gmail.com>
2015-06-30 09:08:21 +0300

    Save work on dynamic templates

--- app/shared/header/HeaderDirective.js
+++ app/shared/header/HeaderDirective.js
@@ -4,12 +4,12 @@
     angular.module('ReadApp').directive('chHeader', ['MenuService', function(MenuService) {
         return {
             restrict: 'E',
+            templateUrl: 'app/shared/header/HeaderView.html',
             link: function (scope) {
                 MenuService.success(function (data) {
                     scope.menus = data.menus;
                 });
-            },
-            templateUrl: 'app/shared/header/HeaderView.html'
+            }
         };
     }]);
 }());
--- app/shared/sidebar/SidebarDirective.js
+++ app/shared/sidebar/SidebarDirective.js
@@ -2,6 +2,9 @@
     'use strict';
 
     angular.module('ReadApp').directive('chSidebar', [function() {
+        var getTemplateUrl = function () {
+        };
+
         return {
             restrict: 'E',
             templateUrl: 'app/shared/sidebar/SidebarView.html'
